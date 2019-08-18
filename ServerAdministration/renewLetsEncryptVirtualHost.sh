221adebcbbecb318051e9fb88cc3d3adcf56dc96
Enno Woortmann <enno@wol-soft.de>
2019-08-18 15:29:41 +0200

    Update renewLetsEncryptVirtualHost.sh

--- ServerAdministration/renewLetsEncryptVirtualHost.sh
+++ ServerAdministration/renewLetsEncryptVirtualHost.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-mv /etc/apache2/sites-enabled/$1.conf ./$1.conf
+mkdir $1.conf
+mv /etc/apache2/sites-enabled/* ./$1.conf
 mkdir $1
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -9,16 +10,16 @@ FILE="/etc/apache2/sites-enabled/$1.conf"
 /bin/cat <<EOM >$FILE
 <VirtualHost *:80>
     ServerName $1
-        DocumentRoot $DIR/$1/
+    DocumentRoot $DIR/$1/
 </VirtualHost>
 EOM
 
 service apache2 reload
-sudo certbot run -a webroot -i apache -w $DIR/$1
+sudo certbot certonly -a webroot -n -w $DIR/$1 -d $1
 
 rm /etc/apache2/sites-enabled/$1.conf
-mv ./$1.conf /etc/apache2/sites-enabled/$1.conf
+mv ./$1.conf/* /etc/apache2/sites-enabled/
+rmdir $1.conf
 rmdir $1
 
-rm /etc/apache2/sites-enabled/${1}-le-ssl.conf
 service apache2 reload
